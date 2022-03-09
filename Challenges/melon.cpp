2c69debd90398e850113000c270bfb4e874c1ff7
JosephParayil <97804798+JosephParayil@users.noreply.github.com>
2022-03-09 15:06:29 -0500

    Add files via upload

---
+++ Challenges/melon.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+void solve() {
+    unsigned int x,y; cin>>x>>y;
+    cout<<(x+y)*2<<'\n';
+}
+
+int main() {
+    int t; cin>>t;
+    while(t--) solve();
+    return 0;
+}
+
+
+
